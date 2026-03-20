fa393f784f6f18d953f7dcd3032a39fd9322f1a2
RomanPetrenkov <roman.tihka2008@gmail.com>
2026-03-20 16:49:44 +0300

    Удалил вложенную папку tkd_bot

--- tkd_bot/tkd_bot/data_base/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('coaches.db', check_same_thread=False)
-cursor = conn.cursor()
-
-conn2 = sqlite3.connect('reservation.db', check_same_thread=False)
-cursor2 = conn2.cursor()
